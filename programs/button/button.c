4997f6f0ed6dd1d0f84b51ff606c6c4b46f71761
patvax0x0@gmail.com <patvax0x0@gmail.com>
2020-08-16 15:28:27 +0200

    Buttontestprogram

--- programs/button/button.c
+++ programs/button/button.c
@@ -3,16 +3,22 @@
 
 #define reg_leds (*(volatile uint8_t*)0xFFFFFFF0)	
 #define reg_button (*(volatile uint8_t*)0xFFFFFFE0)
+#define reg_gpio0_dir (*(volatile uint8_t*)0xFFFFFE01)
+#define reg_gpio0_value (*(volatile uint8_t*)0xFFFFFE00)
+#define reg_gpio1_dir (*(volatile uint8_t*)0xFFFFFE03)
+#define reg_gpio1_value (*(volatile uint8_t*)0xFFFFFE02)
 
 
 void main() {
 	
- 
-    uint8_t btn0 = reg_button;
+	reg_gpio0_dir = (uint8_t)0b11111111;
+	reg_gpio1_dir = (uint8_t)0b11111111;
        
     while (1) { 
-		btn0 = reg_button; 
-		reg_leds = btn0;		
+		btn0 = reg_button;
+		reg_leds = btn0;
 
+		reg_gpio0_value = btn0;
+		reg_gpio1_value = ~btn0;
     } 
 }	
